9446300e3d97c96dbe5f177148460488a28191ac
antoinetteanderson <antoinetteanderson88@gmail.com>
2023-10-24 21:20:10 -0500

    working on question 4

--- scripts/lahmans_baseball_antoinette_anderson.sql
+++ scripts/lahmans_baseball_antoinette_anderson.sql
@@ -40,6 +40,12 @@ ORDER BY salary DESC
 
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
    
+SELECT *
+FROM fielding
+JOIN appearances
+USING (playerid)
+
+   
 -- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
    
 
