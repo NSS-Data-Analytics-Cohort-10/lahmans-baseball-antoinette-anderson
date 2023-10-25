857a31c6d7485cb84bfb38b386ba79125f3b6297
antoinetteanderson <antoinetteanderson88@gmail.com>
2023-10-24 21:02:48 -0500

    skipped 2. finished 3

--- scripts/lahmans_baseball_antoinette_anderson.sql
+++ scripts/lahmans_baseball_antoinette_anderson.sql
@@ -6,10 +6,37 @@ ORDER BY year;
 -- 1871-2016 
 
 -- 2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
-   
+SELECT DISTINCT namefirst, namelast, height, a.teamid, t.name, hg.games 
+FROM people as p
+JOIN appearances as a
+USING (playerid)
+JOIN teams as t
+USING(teamid)
+FULL JOIN homegames as h
+ON p.namefirst = h.games
+ORDER BY height;
+
+
+
+
+
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
-	
+SELECT p.namefirst, p.namelast, MAX(c.yearid) as year, s.schoolname, MAX(ss.salary) AS salary
+FROM people as p
+JOIN collegeplaying as c
+USING(playerid)
+JOIN schools as s
+USING(schoolid)
+JOIN salaries as ss
+USING (playerid)
+WHERE schoolname = 'Vanderbilt University'
+GROUP BY p.namefirst, p.namelast, s.schoolname
+ORDER BY salary DESC
+
+-- David Price with 30 million
+
+
 
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
    
