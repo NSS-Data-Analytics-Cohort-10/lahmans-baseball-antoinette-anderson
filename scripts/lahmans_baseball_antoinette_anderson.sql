31799f62302386898d5af278d075c1c9b53a6dbf
antoinetteanderson <antoinetteanderson88@gmail.com>
2023-10-28 07:17:00 -0500

    finished question 10

--- scripts/lahmans_baseball_antoinette_anderson.sql
+++ scripts/lahmans_baseball_antoinette_anderson.sql
@@ -87,3 +87,12 @@ HAVING COUNT(DISTINCT am.lgid)=2
 
 
 -- 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
+
+SELECT namefirst, namelast, debut, hr
+FROM people as p
+JOIN batting as b
+USING (playerid)
+WHERE b.yearid = 2016
+AND debut <= '2006-12-31'
+AND hr >= '1'
+ORDER by hr DESC;
